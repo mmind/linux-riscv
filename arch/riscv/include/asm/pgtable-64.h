dec2b74caff4d63ef3927710b2156d70a739865f
Heiko Stuebner <heiko@sntech.de>
2021-06-06 09:04:02 +0000

    riscv: Fixup _PAGE_CHG_MASK usage in pgtable
    Mask all attribute bits first and then do a right-shift by
    _PAGE_PFN_SHIFT to get the final PFN value.
    
    Adding '& _PAGE_CHG_MASK' makes the code semantics more accurate.
    
    _PAGE_CHG_MASK was already used in some places, so this
    patch only extends its usage to all involved locations.
    
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>

--- arch/riscv/include/asm/pgtable-64.h
+++ arch/riscv/include/asm/pgtable-64.h
@@ -61,12 +61,12 @@ static inline void pud_clear(pud_t *pudp)
 
 static inline pmd_t *pud_pgtable(pud_t pud)
 {
-	return (pmd_t *)pfn_to_virt(pud_val(pud) >> _PAGE_PFN_SHIFT);
+	return (pmd_t *)pfn_to_virt((pud_val(pud) & _PAGE_CHG_MASK) >> _PAGE_PFN_SHIFT);
 }
 
 static inline struct page *pud_page(pud_t pud)
 {
-	return pfn_to_page(pud_val(pud) >> _PAGE_PFN_SHIFT);
+	return pfn_to_page((pud_val(pud) & _PAGE_CHG_MASK) >> _PAGE_PFN_SHIFT);
 }
 
 static inline pmd_t pfn_pmd(unsigned long pfn, pgprot_t prot)
@@ -76,7 +76,7 @@ static inline pmd_t pfn_pmd(unsigned long pfn, pgprot_t prot)
 
 static inline unsigned long _pmd_pfn(pmd_t pmd)
 {
-	return pmd_val(pmd) >> _PAGE_PFN_SHIFT;
+	return (pmd_val(pmd) & _PAGE_CHG_MASK) >> _PAGE_PFN_SHIFT;
 }
 
 #define mk_pmd(page, prot)    pfn_pmd(page_to_pfn(page), prot)
