eccae433865080ed73def5d256d53e7f7e438dff
Samuel Holland <samuel@sholland.org>
2021-06-13 23:54:42 -0500

    [DO NOT MERGE] misc changes
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- drivers/of/irq.c
+++ drivers/of/irq.c
@@ -554,6 +554,8 @@ void __init of_irq_init(const struct of_device_id *matches)
 		desc->interrupt_parent = of_irq_find_parent(np);
 		if (desc->interrupt_parent == np)
 			desc->interrupt_parent = NULL;
+		pr_notice("of_irq_init: found %pOF with parent %pOF\n",
+			  desc->dev, desc->interrupt_parent);
 		list_add_tail(&desc->list, &intc_desc_list);
 	}
 
@@ -578,9 +580,8 @@ void __init of_irq_init(const struct of_device_id *matches)
 
 			of_node_set_flag(desc->dev, OF_POPULATED);
 
-			pr_debug("of_irq_init: init %pOF (%p), parent %p\n",
-				 desc->dev,
-				 desc->dev, desc->interrupt_parent);
+			pr_notice("of_irq_init: init %pOF with parent %pOF\n",
+				  desc->dev, desc->interrupt_parent);
 			ret = desc->irq_init_cb(desc->dev,
 						desc->interrupt_parent);
 			if (ret) {
